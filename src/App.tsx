a6414c8909467f14d2fdbc593d27cadeb3f4b1a1
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-01-01 21:01:39 -0500

    create Category component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react'
+import { Category } from './components/Category'
 
 const App: FC = () => {
   return (
     <div>
+      <Category />
       <h1>HOLA MUNDO</h1>
     </div>
   )
---
+++ src/components/Category/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import { Anchor, Image } from './styled'
+
+const DEFAULT_IMAGE = 'https://imgur.com/dJa0Hpl.png'
+
+type AppProps = {
+  cover?: string,
+  path?: string,
+  emoji?: string
+}
+
+export const Category: FC<AppProps> = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => {
+  return (
+    <Anchor href={path}>
+      <Image src={cover} />
+      {emoji}
+    </Anchor>
+  )
+}
