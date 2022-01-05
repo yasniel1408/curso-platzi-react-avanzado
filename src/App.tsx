5261915058ea747d61145976e472b36af99582ed
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-01-04 22:57:29 -0500

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import React, { FC } from 'react';
-import { GlobalStyle } from './styles/GlobalStyles';
-import { ListOfCategory } from './components/ListOfCategory';
-import { ListOfPhotoCards } from './components/ListOfPhotoCards';
-import { Logo } from './components/Logo';
+import { FC } from 'react';
+import { GlobalStyle } from 'styles/GlobalStyles';
+import { ListOfCategory } from 'components/ListOfCategory';
+import { ListOfPhotoCards } from 'components/ListOfPhotoCards';
+import { Logo } from 'components/Logo';
 
-const App: FC = () => {
-  return (
-    <div>
-      <GlobalStyle />
-      <Logo />
-      <ListOfCategory />
-      <ListOfPhotoCards />
-    </div>
-  );
-};
-
-export default App;
+export const App: FC = () => (
+  <div>
+    <GlobalStyle />
+    <Logo />
+    <ListOfCategory />
+    <ListOfPhotoCards />
+  </div>
+);
