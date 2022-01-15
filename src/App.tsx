fec1a56a62c8d7d04a3dd09b56d78543183fd6b1
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-01-15 00:51:21 -0500

    redirect when login user

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Logo } from 'components/Logo';
 import { Home } from 'pages/Home';
 import { FC } from 'react';
 import { GlobalStyle } from 'styles/GlobalStyles';
-import { Router } from '@reach/router';
+import { Redirect, Router } from '@reach/router';
 import { Details } from 'pages/Details';
 import { NavBar } from 'components/NavBar';
 import { Favs } from 'pages/Favs';
 import { User } from 'pages/User';
 import { UserAuthentication } from 'pages/UserAuthentication';
 import { useAuth } from 'context/hooks/useAuth';
+import { NotFound } from 'pages/NotFound';
 
 export const App: FC = () => {
   const value: any = useAuth();
@@ -18,21 +19,18 @@ export const App: FC = () => {
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pets/:categoryId" />
         <Details path="/detail/:categoryId" />
+        {!value.isAuth && <UserAuthentication path="/login" />}
+        {!value.isAuth && <Redirect from="/favs" to="/login" />}
+        {!value.isAuth && <Redirect from="/user" to="/login" />}
+        {value.isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      {value.isAuth ? (
-        <Router>
-          <Favs path="/favs" />
-          <User path="/user" />
-        </Router>
-      ) : (
-        <Router>
-          <UserAuthentication path="/favs" />
-          <UserAuthentication path="/user" />
-        </Router>
-      )}
+
       <NavBar />
     </div>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,4 @@
+import { RouteComponentProps } from '@reach/router';
+import { FC } from 'react';
+
+export const NotFound: FC<RouteComponentProps> = () => <h1>Esta página no existe! :(</h1>;
