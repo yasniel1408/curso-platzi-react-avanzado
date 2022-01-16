066734b04e78678607bed5e8ccc99b0a352f0068
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-01-15 20:30:51 -0500

    test in sypress

--- cypress/integration/petgram/test.spec.js
+++ cypress/integration/petgram/test.spec.js
@@ -1,6 +1,18 @@
-/* eslint-disable no-undef */
 describe('Mi primer test', () => {
-  it('Debe de cargar la pagina', () => {
-    expect(true).to.equal(true);
+  it('Ver si la app funciona', () => {
+    cy.visit('https://petgram-yasniel1408.vercel.app/');
+  });
+  it('Navegar a una categoria y vemos fotos', () => {
+    cy.visit('/pets/1');
+    cy.get('article');
+  });
+  it('Ver si podemos navegar con la nav a la home', () => {
+    cy.visit('/pets/1');
+    cy.get('nav a').first().click();
+    cy.url().should('include', '/');
+  });
+  it('Los usuarios no registrados ven el form de inicion de session al ir a /favs', () => {
+    cy.visit('/favs');
+    cy.get('form').should('have.length', 2);
   });
 });
